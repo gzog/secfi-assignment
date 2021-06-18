d1c65af23e56352f2944523df89a7113ed59e0ec
George Zografos <george.p.zografos@gmail.com>
2021-06-18 19:10:36 +0200

    Add mutation to schema

--- service/app/schema.py
+++ service/app/schema.py
@@ -16,8 +16,56 @@ class Query(graphene.ObjectType):
         return User.objects.all()
 
     def resolve_user(self, info, **kwargs):
-        username = kwargs.get('username')
+        username = kwargs.get("username")
         return User.objects.get_or_none(username=username)
+
+
+class UserCreateMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        year = graphene.Int(required=True)
+
+    user = graphene.Field(UserType)
+
+    def mutate(self, info, title, year):
+        user = User.objects.create(title=title, year=year)
+        return UserCreateMutation(user=user)
+
+
+class UserUpdateMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        year = graphene.Int(required=True)
+        id = graphene.ID(required=True)
+
+    user = graphene.Field(UserType)
+
+    def mutate(self, info, id, title, year):
+        user = User.objects.get(pk=id)
+        if title is not None:
+            user.title = title
+        if year is not None:
+            user.year = year
+        user.save()
+        return UserUpdateMutation(user=user)
+
+
+class UserDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    user = graphene.Field(UserType)
+
+    def mutate(self, info, id):
+        user = User.objects.get(pk=id)
+        user.delete()
+        return UserDeleteMutation(user=None)
+
+
+class Mutation:
+    create_user = UserCreateMutation.Field()
+    update_user = UserUpdateMutation.Field()
+    delete_user = UserDeleteMutation.Field()
 
 
 schema = graphene.Schema(query=Query)
