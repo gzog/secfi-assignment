290f8cb3145cd1866450bdcd5c31b73b1882ca68
George Zografos <george.p.zografos@gmail.com>
2021-06-18 17:20:24 +0200

    Create model and basic views

--- service/app/models.py
+++ service/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+    avatar = models.CharField(max_length=1024)
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return self.username
--- service/app/views.py
+++ service/app/views.py
@@ -1,3 +1,19 @@
+from django.views import View
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+
+class UserList(View):
+    http_method_names = ['get']
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({})
+
+
+class UserDetail(View):
+
+    http_method_names = ['get']
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({})
