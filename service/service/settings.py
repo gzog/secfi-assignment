0e01cc6a571bf51bdf123bfc28c20667f10cf064
George Zografos <george.p.zografos@gmail.com>
2021-06-18 16:47:30 +0200

    Remove additional unused middleware

--- service/service/settings.py
+++ service/service/settings.py
@@ -41,9 +41,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'service.urls'
