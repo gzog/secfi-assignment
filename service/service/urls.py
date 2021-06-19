eb6f9bf22f62c879a76c47bff7fbee026b935f77
George Zografos <george.p.zografos@gmail.com>
2021-06-19 08:31:52 +0200

    Do not show Graphql explorer when DEBUG=False

--- service/service/urls.py
+++ service/service/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.urls import path
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path("graphql", GraphQLView.as_view(graphiql=True)),
+    path("", GraphQLView.as_view(graphiql=settings.DEBUG)),
 ]
